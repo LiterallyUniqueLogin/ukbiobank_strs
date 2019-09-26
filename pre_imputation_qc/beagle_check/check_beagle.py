f0190e777ab561279c3e8603d2322baf2b053bc8
Jonathan Margoliash <jmargoli@tscc-login2.sdsc.edu>
2019-09-26 11:25:11 -0700

    Working on check beagle script

---
+++ pre_imputation_qc/beagle_check/check_beagle.py
@@ -0,0 +1,107 @@
+import sys
+
+with open(sys.argv[1], 'w') as outfile:
+	lineNumber = 0
+	for line in sys.stdin:
+		if line[0] == '#':
+			continue
+		lineNumber += 1
+		print("\rReading varaint {}".format(lineNumber), end ='')
+		tokens = line.split()
+		chrom = tokens[0]
+		pos = tokens[1]
+		n_alt = len(tokens[4].split(','))
+		alt_freqs = [float(x) for x in tokens[7].split(';')[1][3:].split(',')]
+		if n_alt != len(alt_freqs):
+			outfile.write('Wrong number of alt_freqs {}:{}\n'.format(chrom, pos))
+		n_samples = len(tokens) - 9
+		alt_counts = [0]*n_alt
+		sample_num = 0
+		for sample in tokens[9:]:
+			sample_num += 1
+			splits = sample.split(':')
+			GT = splits[0]
+			DSs = [float(x) for x in splits[1].split(',')]
+
+			#modified for APs
+			AP1s = [float(x) for x in splits[2].split(',')]
+			AP2s = [float(x) for x in splits[3].split(',')]
+			GPs = [float(x) for x in splits[4].split(',')]
+
+			if n_alt != len(DSs):
+				outfile.write('Wrong number of dosages sample #{} {}:{}\n'.format(sample_num, chrom, pos))
+
+			#modified for APs
+			if n_alt != len(AP1s) or n_alt != len(AP2s):
+				outfile.write('Wrong number of allele probs for sample #{} {}:{}\n'.format(sample_num, chrom, pos))
+			if sum(AP1s) - 1 >= 0.04 or sum(AP2s) - 1 >= 0.04:
+				outfile.write('Too much probability in alternate alleles for sample #{} {}:{}\n'.format(sample_num, chrom, pos))
+
+			if (n_alt + 1) * (n_alt + 2) / 2 != len(GPs):
+				outfile.write('Wrong number of GPs sample #{} {}:{}\n'.format(sample_num, chrom, pos))
+			if abs(sum(GPs) - 1) >= 0.04:
+				outfile.write('GPs sum to {} for sample #{} {}:{}\n'.format(sum(GPs), sample_num, chrom, pos))
+			
+			alleles = [int(x) for x in GT.split('|')]
+			for allele in alleles:
+				if allele > 0:
+					alt_counts[allele - 1] += 1
+			dosage_per_allele = [0]*n_alt
+			highestGP = 0
+			gpCount = -1
+			bestGTs=[]
+			for a1 in range(n_alt + 1):
+				for a2 in range(a1 + 1):
+					gpCount += 1
+					#modified for APs
+					if a1 == 0:
+						a1Prob = 1 - sum(AP1s)
+					else:
+						a1Prob = AP1s[a1 - 1]
+					if a2 == 0:
+						a2Prob = 1 - sum(AP2s)
+					else:
+						a2Prob = AP2s[a2 - 1]
+
+					maxExpectedProb = (a1Prob+0.01)*(a2Prob+0.01)
+					minExpectedProb = (a1Prob-0.01)*(a2Prob-0.01)
+
+					if a1 != a2:
+						if a1 == 0:
+							a2Prob = 1 - sum(AP2s)
+						else:
+							a2Prob = AP2s[a1 - 1]
+						if a2 == 0:
+							a1Prob = 1 - sum(AP1s)
+						else:
+							a1Prob = AP1s[a2 - 1]
+						maxExpectedProb += (a1Prob+0.01)*(a2Prob+0.01)
+						minExpectedProb += (a1Prob-0.01)*(a2Prob-0.01)
+
+					if not (maxExpectedProb + 0.01 >= GPs[gpCount] and (GPs[gpCount] >= minExpectedProb - 0.01)):
+						outfile.write("GP ({}) doesn't match corresponding APs ({}, {}) \
+								for alleles {}, {} for sample #{} {}:{}\n".format(\
+							GPs[gpCount], a1Prob, a2Prob, a1, a2, sample_num, chrom, pos))
+
+					if GPs[gpCount] - highestGP >= 0.04:
+						highestGP = GPs[gpCount]
+						bestGTs = ['{}|{}'.format(a2, a1), '{}|{}'.format(a1, a2)]
+					elif GPs[gpCount] - highestGP > -0.04:
+						bestGTs.append('{}|{}'.format(a2, a1))
+						bestGTs.append('{}|{}'.format(a1, a2))
+					if a1 > 0:
+						dosage_per_allele[a1 - 1] += GPs[gpCount]
+					if a2 > 0:
+						dosage_per_allele[a2 - 1] += GPs[gpCount]
+			
+			#Check GT matches the highest GP
+			if GT not in bestGTs:
+				outfile.write('Wrong GT (GPs {} , derived from GPs {} , Beagle output {}) for sample #{} {}:{}\n'.format(GPs, bestGTs, GT, sample_num, chrom, pos))
+			#Check dosages are correct
+			for n in range(n_alt):
+				if abs(DSs[n] - dosage_per_allele[n]) >= 0.04:
+					outfile.write('Wrong dosage (alt allele {}, derived from GPs {}, Beagle output {}) for sample #{} {}:{}\n'.format(n, dosage_per_allele[n], DSs[n], sample_num, chrom, pos))
+		#Check alt_freqs are correct
+		for n in range(n_alt):
+			if abs(alt_freqs[n] - alt_counts[n]/n_samples/2) >= 0.05:
+					outfile.write('Wrong alternate allele freq (alt allele: {}, Derived from GTs {}, Beagle output {}) for {}:{}\n'.format(n, alt_counts[n]/n_samples/2, alt_freqs[n], chrom, pos))
