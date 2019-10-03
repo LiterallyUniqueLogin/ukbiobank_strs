1cb38b30fac7585920d1daad589288c33dedd5f7
Jonathan Margoliash <jmargoli@eng.ucsd.edu>
2019-10-03 12:52:53 -0700

    Rearranged the filesystem so that original files are in the group dir but everything else stays in mine

--- original/bgen_original/hap/downloadAll.sh
+++ original/bgen_original/hap/downloadAll.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Requires the ukbgene tool in this directory
+#And .ukbkey in this directory - the credential used for downloading
 
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 ;	do 
 	./ukbgene hap -c$i 
