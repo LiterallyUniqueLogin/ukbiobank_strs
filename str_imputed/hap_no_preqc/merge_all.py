459869b657dc2104c861d413c58b4dac76596793
Jonathan Margoliash <jmargoli@eng.ucsd.edu>
2019-10-29 13:55:14 -0700

    Will this script finally make merging a reality?

--- str_imputed/hap_no_preqc/merge_all.py
+++ str_imputed/hap_no_preqc/merge_all.py
@@ -3,23 +3,29 @@ import os
 import sys
 import datetime
 import time
+#import numpy as np
 
 if len(sys.argv) != 2:
         print("Expecting exactly 1 argument, the chromosome number")
         exit(-1)
 chr = sys.argv[1]
 
+#set n_samples for all files and then last file
+samples_per_file = 1000
+samples_last_file = 409
+n_files = 488
+n_files_minus_one = n_files - 1
+
 try:
 	startup_start = time.time()
 	input_vcfs = []
 	vcf_names = []
 	output = None
-	for i in range(0, 488):
+	sample_block_len = len('\t0|0:0:0:0')
+	#min_lens = np.full((488), 1) #[1] * 488
+	for i in range(0, n_files):
 		file_name = glob.glob("{}/str_imputed/hap_no_preqc/vcf_batches/chr{}_samples_{}_*.vcf" .format(\
-			os.environ['UKB'], chr, i*1000 + 1))
-		if len(file_name) == 0: #only for testing!
-			continue
-		file_name = file_name[0] 
+			os.environ['UKB'], chr, i*samples_per_file + 1))[0]
 		vcf_names.append(file_name)
 		input_vcfs.append(open(file_name, 'br'))
 
@@ -47,10 +53,12 @@ try:
 
 	byte = first_vcf.read(1)
 	tab_count = 0
-	while tab_count < 9: #Read till the first sample ID
+	while True: #Read till the first sample ID
 		metadata_lines += byte
 		if byte == b'\t':
 			tab_count += 1
+			if tab_count == 9:
+				break
 		byte = first_vcf.read(1)
 
 	#Confirm all other headers are the same except for maybe the date line
@@ -61,18 +69,18 @@ try:
 		i += 1
 		metadata = file.read(metadata_len)
 		metadata = metadata.decode('us-ascii').split('\n')
-		if len(metadata) != len(metadata_lines):
-			print('File {} has obviously different metadata than file {}'.format(vcf_names[i], vcf_names[0]))
-			exit(-1)
+		#if len(metadata) != len(metadata_lines):
+		#	print('File {} has obviously different metadata than file {}'.format(vcf_names[i], vcf_names[0]))
+		#	exit(-1)
 
-		for line1, line2 in zip(metadata_lines, metadata):
-			if not ('filedate' in line1 and 'filedate' in line2) and line1 != line2:
-				print('Found file {} with different metadata than file {}'.format(vcf_names[i], vcf_names[0]))
-				print('line1', line1)
-				print()
-				print('line2', line2)
-				exit(-1)
-	print('All files have the same metadata')
+		#for line1, line2 in zip(metadata_lines, metadata):
+		#	if not ('filedate' in line1 and 'filedate' in line2) and line1 != line2:
+		#		print('Found file {} with different metadata than file {}'.format(vcf_names[i], vcf_names[0]))
+		#		print('line1', line1)
+		#		print()
+		#		print('line2', line2)
+		#		exit(-1)
+	#print('All files have the same metadata')
 
 	output = open("{}/str_imputed/hap_no_preqc/vcfs/chr{}.vcf".format(os.environ['UKB'], chr), 'wb')
 
@@ -109,9 +117,12 @@ try:
 		
 		byte = vcf.read(1)
 		while byte != b'\n':
+			#if byte == b'\t':
+			#	min_lens[i] += 1	
 			output.write(byte)
 			byte = vcf.read(1)
 	output.write(b'\n')
+	#min_lens = [x*sample_block_len - 1 for x in min_lens]
 
 	print("startup time: {:.2}s".format(time.time() - startup_start))
 	
@@ -121,14 +132,20 @@ try:
 	all_variants_start = time.time()
 	n_variants = 0
 	#minimum length of the data portion of a line
+	'''
 	sample_block_len = len('\t0|0:0:0:0')
 	min_len = 904*sample_block_len - 1
+	min_lens = [min_len] * 488
+	'''
+	min_len = samples_per_file*sample_block_len - 2
+	min_len_last_file = samples_last_file*sample_block_len - 2
 	while True: #iterate over all variants
 		if n_variants % 10 == 0:
 			variant_start = time.time()
 		byte = first_vcf.read(1)
-		fixed_field_bytes = 1
+		fixed_field_bytes = 0
 		if byte == b"": #check if we're done
+			print("Checking if we're done")
 			for i, vcf in enumerate(input_vcfs[1:]):
 				i += 1
 				byte = vcf.read(1)
@@ -138,13 +155,13 @@ try:
 			print("Done")
 			exit(0)
 		tab_count = 0
+		info = bytearray()
 		#read the fixed fields from the first file
 		while tab_count < 9:
+			fixed_field_bytes += 1
 			if byte == b'\t':
 				tab_count += 1
-				if tab_count == 7:
-					info = bytearray()
-				elif tab_count == 8:
+				if tab_count == 8:
 					if b'IMP' in info:
 						output.write(b'IMP')
 					else:
@@ -156,24 +173,43 @@ try:
 				info += byte
 				
 			byte = first_vcf.read(1)
-			fixed_field_bytes += 1
+			if byte == b"":
+				print("Didn't expect to be done! In first vcf")
+				1/0
 		#emit the sample data from the first file
+		output.write(byte)
+		#output.write(first_vcf.read(min_lens[0]))
+		output.write(first_vcf.read(min_len))
+		byte = first_vcf.read(1)
 		while byte != b'\n':
+			if byte == b"":
+				print("Didn't expect to be done! In first vcf")
+				1/0
 			output.write(byte)
 			byte = first_vcf.read(1)
 		#emit the sample data from all other files (assume the fixed fields are correct)
-		#for i, vcf in enumerate(input_vcfs[1:]):
+		i = 0
 		for vcf in input_vcfs[1:]:
+			i += 1
 			output.write(b'\t')
 			#skip fixed fields. Assume that all fixed fields are exactly the same length
 			#(seems to be true, all info fields are fixed precision)
-			vcf.read(fixed_field_bytes)
+			vcf.seek(fixed_field_bytes, 1)
 
 			#output all sample data
-			output.write(byte)
-			output.write(vcf.read(min_len))
+			#data = vcf.read(min_len)
+			if i != n_files_minus_one:
+				output.write(vcf.read(min_len))
+			else:
+				output.write(vcf.read(min_len_last_file))
+			#print(data.decode('us-ascii'))
+			#output.write(data)
+			#output.write(vcf.read(min_lens[i]))
 			byte = vcf.read(1)
 			while byte != b'\n':
+				if byte == b"":
+					print("Didn't expect to be done! In vcf {}".format(vcf_names[i]))
+					1/0
 				output.write(byte)
 				byte = vcf.read(1)
 				
