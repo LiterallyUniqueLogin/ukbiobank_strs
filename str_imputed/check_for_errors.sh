09eecbd5e1a35942411af2c7bc6e2763e1149d29
Jonathan Margoliash <jmargoli@eng.ucsd.edu>
2020-02-03 14:15:02 -0800

    Reworked the error checking script to be more useful

--- str_imputed/check_for_errors.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -z "$1" ] ; then
-        echo "Didn't give first argument - should be chrom number"
-        exit -1
-fi
-
-grep -h -b100 -v INPUT $UKB/str_imputed/runs/$1/batches/output/*.e* | vim -
-
---
+++ str_imputed/check_for_pbs_script_errors.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ -z "$1" ] ; then
+        echo "Didn't give first argument - should be run name"
+        exit -1
+fi
+
+#Look for any line that isn't the ouput of a time command
+#Isn't just a log of the inputs to the script
+#And isn't a blank line
+for file in $UKB/str_imputed/runs/first_pass/batches/output/*.e* ; do
+	if [ ! -z "$(grep -l -v -P '(INPUT)|(real\t[0-9]+m[0-9.]+s)|(user\t[0-9]+m[0-9.]+s)|(sys\t[0-9]+m[0-9.]+s)|(^$)' $file)" ] ; then
+		echo "Error in file $file:"
+		grep -n -v -P '(INPUT)|(real\t[0-9]+m[0-9.]+s)|(user\t[0-9]+m[0-9.]+s)|(sys\t[0-9]+m[0-9.]+s)|(^$)' $file
+	fi
+done 
+
