64a64bc1840ac787974625aa843488071f3f187c
Jonathan Margoliash <jmargolpeople@gmail.com>
2020-03-03 14:24:04 -0800

    Done with default filters for now

--- sample_qc/scripts/white_brits_list.py
+++ sample_qc/scripts/white_brits_list.py
@@ -1,3 +1,11 @@
-import list_from_sample_qc as list
+"""
+List white samples with British ancestry.
 
-list.produceListFromSampleQC('white_brits', True, 23)
+See UKB nature paper supplementary information section S 3.4
+https://static-content.springer.com/esm/art%3A10.1038%2Fs41586-018-0579-z/MediaObjects/41586_2018_579_MOESM1_ESM.pdf
+"""
+
+import list_from_sample_qc as sqc
+
+
+sqc.produce_list_from_sample_qc('white_brits', True, column=23)
