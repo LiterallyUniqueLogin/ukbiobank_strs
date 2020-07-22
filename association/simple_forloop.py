c03b16fad5c8a30b0b7eef52da5115c93bd104c3
Jonathan Margoliash <jmargolpeople@gmail.com>
2020-07-22 11:24:55 -0700

    Reorganized the main_dataset directory. Rewrote the extractField.py script to extract height data

--- association/simple_forloop.py
+++ association/simple_forloop.py
@@ -8,10 +8,12 @@ and pumps them in to scipy
 """
 
 import argparse
+from Typing import Tuple
 import os
 
-import cyvcf2 
+import cyvcf2
 import numpy as np
+from numpy.typing import ArrayLike
 import scipy
 
 ukb = os.environ['UKB']
@@ -32,18 +34,37 @@ def main():  # noqa: D103
 				f'vcfs/strs_only/chr{chrom}.vcf.gz')
     """
 
+
+def load_covars():
+    """
+    Load the sex and population PC covariates.
+
+    Returns
+    -------
+    Tuple[ArrayLike[int], ArrayLike[int], ArrayLike[float, float]] :
+        ids, sex, pcs. The id of each entry in the sex array and each row
+        in the pcs array is the corresponding entry in the ids array.
+
+    Notes
+    -----
+    If interested in the source of this data, read the READMEs in the
+    directories of the loaded files.
+    """
     pcs = np.loadtxt(
-        f'{ukb}/misc_data/EGA/ukb_sqc_v2.txt'
+        f'{ukb}/misc_data/EGA/ukb_sqc_v2.txt',
         usecols=range(-43, -3)
     ) #156MB
     ids_and_sex = np.loadtxt(
         f'{ukb}/microarray/ukb46122_cal_chr1_v2_s488282.fam',
         usecols=(0, 4),
         dtype=int,
-    ) 
-    ids = ids_and_sex[:,0] #each entry here is the id of the corresponding row of pcs
-    sex = ids_and_sex[:,1] #1 = M, 2 = F
-    
+    )
+    ids = ids_and_sex[:, 0] #each entry here is the id of the corresponding row of pcs
+    sex = ids_and_sex[:, 1] #1 = M, 2 = F
+    return ids, sex, pcs
+
+
+def
 
 if __name__ == "__main__":
     main()
--- main_dataset/scripts/extractField.py
+++ main_dataset/scripts/extractField.py
@@ -1,16 +1,43 @@
+"""
+Extract a subset of fields from the ukb29170.csv file by name.
+
+Always also extracts the id field.
+Output form is csv
+"""
+
 import csv
+import os
 import sys
 
-if len(sys.argv) != 2:
-	print("Excpecting exactly one argument: the column header to look for")
-	exit(-1)
+if len(sys.argv) < 2:
+    print("Excpecting at least one argument: the column header(s) to look for")
+    sys.exit(-1)
 
-header = sys.argv[1]
+fields = sys.argv[1:]
+fields.insert(0, "eid")
 
-with open('ukb29170.csv') as csvfile:
-	reader = csv.reader(csvfile, delimiter=',', quotechar="\"")
-	line1 = next(reader)
-	idx = line1.index(header)
-	for line in reader:
-		print(line[idx])
+ukb = os.environ['UKB']
+
+def print_line(idxs, line):
+    for count, idx in enumerate(idxs):
+        if count != len(idxs) - 1:
+            print(line[idx], end=",")
+        else:
+            print(line[idx])
+
+
+with open(f'{ukb}/main_dataset/raw_data/ukb29170.csv') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar="\"")
+    header = next(reader)
+    field_idxs = []
+    for field in fields:
+        # this will raise an error if the field isn't found
+        field_idxs.append(header.index(field))
+
+    # print the header
+    print_line(field_idxs, header)
+
+    # Extract the info
+    for line in reader:
+        print_line(field_idxs, line)
 
