295feda0d48e692829917c35b1afab7174d0b685
Jonathan Margoliash <jmargolpeople@gmail.com>
2020-07-24 14:02:40 -0700

    Debugging the first imputation script. Now have a stable indication of what software environment I'm using for this analysis

--- association/simple_forloop.py
+++ association/simple_forloop.py
@@ -11,10 +11,12 @@ import argparse
 import os
 import os.path
 import sys
+import time
 
 import cyvcf2
 import pandas as pd
 import numpy as np
+from statsmodels.regression.linear_model import OLS
 
 ukb = os.environ['UKB']
 
@@ -30,6 +32,7 @@ def load_covars(readme):
         A categorical column 'id' which serves as the logical index
         An integer column 'sex' with M = 1 and F = 2
         Float columns pc1 ... pc40 for the population structure pcs
+        and an 'intercept' column of ones
 
     Notes
     -----
@@ -53,6 +56,7 @@ def load_covars(readme):
 
     floc = f'{ukb}/misc_data/EGA/ukb_sqc_v2.txt'
     cols = list(range(25, 65))
+    pc_col_names = list(f"pc{col}" for col in range(1, 41))
     readme.write(
         f"Adding PC covariates 1-40. File: {floc}, cols: {cols}. Participants in same "
         f"order as previous file.\n"
@@ -61,13 +65,20 @@ def load_covars(readme):
         floc,
         sep=" ",
         header=None,
-        names=list(f"pc{col}" for col in range(1, 41)),
+        names=pc_col_names,
         usecols=cols,
         dtype=float
     ) #156MB
 
+    const = np.ones((len(pcs.index), 1))
+    const_df = pd.DataFrame(const, columns=["intercept"], dtype="int16")
+    indep_col_names = ['intercept', 'sex']
+    indep_col_names.extend(pc_col_names)
     try:
-        return pd.concat((ids_and_sex, pcs), axis=1)
+        return (
+            pd.concat((ids_and_sex, pcs, const_df), axis=1),
+            indep_col_names
+        )
     finally:
         print("done")
 
@@ -142,14 +153,15 @@ def load_bilirubin(df, readme):
     print("Loading bilirubin ... ", end="", flush=True)
     floc = f'{ukb}/main_dataset/extracted_data/bilirubin.csv'
     readme.write(
-        f"Adding direct and total bilirubin phenotypes. File: {floc}. "
+        f"Adding direct, total and indirect bilirubin phenotypes. File: {floc}. "
         f"Only recording first visit's value, regardless of whether "
         f"it is not present and/or there is a follow up visit value. "
         f"NaNing total values where direct is missing (or visa versa) "
         f"as an overly conservative approach to making sure we have clean "
         f"data as a I don't know why we'd be missing one but not both. "
         f"This effectively cuts down 70k total bilirubin measurements "
-        f"which don't have direct counterparts\n"
+        f"which don't have direct counterparts. The indirect phenotype"
+        f" is simply calculated as total - direct.\n"
     )
     bilirubin_df = pd.read_csv(
         floc,
@@ -165,6 +177,12 @@ def load_bilirubin(df, readme):
     total_nan = np.isnan(bilirubin_df['total bilirubin'])
     bilirubin_df.loc[direct_nan, 'total bilirubin'] = np.nan
     bilirubin_df.loc[total_nan, 'direct bilirubin'] = np.nan
+
+    # Add indirect:
+    bilirubin_df['indirect bilirubin'] = (
+        bilirubin_df['total bilirubin'] -
+        bilirubin_df['direct bilirubin']
+    )
 
     """
     both_nan = np.logical_and(direct_nan, total_nan)
@@ -192,11 +210,17 @@ def main():  # noqa: D103
         'association_run_name',
         help="The name to give this association run"
     )
+    parser.add_argument(
+        'regions',
+        help=('comma separated list with elements of the form chr:start-end '
+              'where chrom is mandatory but the rest are not')
+    )
     args = parser.parse_args()
 
     sample_filtering_run_name = args.sample_filtering_run_name
     imputation_run_name = args.imputation_run_name
     association_run_name = args.association_run_name
+    regions = args.regions.split(',')
 
     assoc_dir = f'{ukb}/association/runs/{association_run_name}'
     if os.path.exists(assoc_dir):
@@ -209,7 +233,10 @@ def main():  # noqa: D103
     with open(f"{assoc_dir}/REAMDE", 'w') as readme, \
             open(f"{assoc_dir}/run.log", 'w') as log, \
             open(f"{assoc_dir}/results.txt", 'w') as results:
-        df = load_covars(readme)
+
+        readme.write(f"Run args: {args}")
+
+        df, indep_vars = load_covars(readme)
         df = load_height(df, readme)
         df = load_bilirubin(df, readme)
 
@@ -257,7 +284,7 @@ def main():  # noqa: D103
         # How can a permutation test (for a QQ plot) still be above the line?
 
         readme.write(f"Using str calls from imputed run {imputation_run_name}\n")
-        readme.wrtie(
+        readme.write(
             "Filtering calls whose phased genotype probability as estimated "
             "by Beagle is less than 0.9. Number filtered this way is the "
             "#samples_GP_filtered column in results.txt"
@@ -274,32 +301,83 @@ def main():  # noqa: D103
         readme.write("Performing linear association against listed covariates"
                      " and average allele length\n")
 
+
+        # Add a column that can be overwritten each step for the genotypes
+        nans = np.empty((len(df.index), 1))
+        nans[:] = np.nan
+        nans_df = pd.DataFrame(nans, columns=['gt'])
+        df = pd.concat((df, nans_df), axis=1)
+
+        # covariate columns:
+        dep_vars = [
+            'height',
+            'total bilirubin',
+            'direct bilirubin',
+            'indirect bilirubin'
+        ]
+
         #Write results header
         results.write("chrom pos #samples_GP_filtered #filtered_rare_alleles")
+        for dep_var in dep_vars:
+            results.write(f" p_{dep_var} coeff_{dep_var}")
+        results.write("\n")
+
+        n_loci = 0
+        batch_time = 0
+        batch_size = 50
+        total_time = 0
         # 2 for bilirubin, 3 for height
         #TODO for sex chroms take into account ploidy
-        for chrom in 2, 3:
+        samples_set_up = False
+
+        for region in regions:
+            print(f"Writing out results for region {region}. See the log for "
+                  f"updates.")
+            chrom = region.split(':')[0]
             vcf_floc = (f'{ukb}/str_imputed/runs/{imputation_run_name}/'
                         f'vcfs/strs_only/chr{chrom}.vcf.gz')
-            for locus in cyvcf2.VCF(vcf_floc):
+            vcf = cyvcf2.VCF(vcf_floc)
+
+            # order the observations in df
+            # based on the order of samples in the first vcf
+            if not samples_set_up:
+                vcf_samples = list(sample.split("_")[0] for sample in
+                                   vcf.samples)
+                samples_df = pd.DataFrame(
+                    vcf_samples,
+                    columns=['id'],
+                    dtype='int'
+                )
+                df = pd.merge(samples_df, df, how='left', on='id')
+
+            vcf_region = vcf(region)
+            for locus in vcf_region:
+                # setup for this locus
+                df['gt'] = np.nan
+                n_loci += 1
+                start_time = time.time()
                 results.write(f"{locus.CHROM} {locus.POS}")
+
                 # gt entries are sequence allele indexes
-                # convert to float so we can use np.nan
-                gt = locus.genotype.array()[:2].astype(float)
+                gt = locus.genotype.array()[:, :2]
                 seq_allele_lens = [len(allele) for allele in locus.ALT]
                 seq_allele_lens.insert(0, len(locus.REF))
 
-                # filter calls whose phased genotype probability
-                # as estimated by Beagle is below .9
+                # get Beagle genotype probability
                 aps = []
                 for copy in 1, 2:
                     ap = locus.format(f'AP{copy}')
                     ref_prob = np.maximum(0, 1 - np.sum(ap, axis=1))
+                    ref_prob = ref_prob.reshape(-1, 1)
                     ap = np.concatenate((ref_prob, ap), axis=1)
-                    ap = ap[:, gt[:, (copy - 1)]]
+                    ap = ap[np.arange(ap.shape[0]), gt[:, (copy - 1)]]
                     aps.append(ap)
-                gp = np.mult(aps[0], aps[1])
+                gp = np.multiply(aps[0], aps[1])
 
+                # filter calls whose phased genotype probability
+                # as estimated by Beagle is below .9
+                # convert to float so we can use np.nan
+                gt = gt.astype(float)
                 filtered_samples = gp < .9
                 gt[filtered_samples, :] = np.nan
                 n_filtered_samples = np.sum(filtered_samples)
@@ -310,7 +388,7 @@ def main():  # noqa: D103
                     gt[gt == seq_allele_idx] = seq_allele_len
 
                 # filter length alleles with too few occurrences
-                len_alleles, counts = np.unique(gt, return_counts=True)[1]
+                len_alleles, counts = np.unique(gt, return_counts=True)
                 filtered_rare_alleles = 0
                 for len_allele_idx, len_allele in enumerate(len_alleles):
                     if (counts[len_allele_idx] > 0 and
@@ -319,8 +397,39 @@ def main():  # noqa: D103
                         filtered_rare_alleles += 1
                 results.write(f" {filtered_rare_alleles}")
 
+                # set gts for regression
+                """
                 avg_len = np.sum(gt, axis=1)/2
+                print(avg_len.shape, avg_len)
+                print(df['gt'].shape, df['gt'])
+                df['gt'] = avg_len
+                """
+                df['gt'] = np.sum(gt, axis=1)/2
+
+                #do da regression
+                for dep_var in dep_vars:
+                    model = OLS(
+                        df[dep_var],
+                        df[indep_vars],
+                        missing='drop'
+                    )
+                    reg_result = model.fit()
+                    pval = reg_result.pvalues['gt']
+                    coef = reg_result.params['gt']
+                    results.write(f" {pval} {coef}")
+
+                # output results
                 results.write("\n")
+                duration = time.time() - start_time
+                total_time += duration
+                batch_time += duration
+                if n_loci % batch_size == 0:
+                    log.write(
+                        f"time/locus (last {batch_size}): "
+                        f"{batch_time/batch_size}s\n"
+                        f"time/locus ({n_loci} total loci): {total_time/n_loci}s\n"
+                    )
+                    batch_time = 0
 
 
 if __name__ == "__main__":
