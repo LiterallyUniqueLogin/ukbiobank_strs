757491c6a905bea269d6adb9188560bec5b525ff
Jonathan Margoliash <jmargolpeople@gmail.com>
2020-08-13 15:08:09 -0700

    Refactored plotting code to make it easier to add Manhattan plots

--- association/make_plots.py
+++ association/make_plots.py
@@ -7,29 +7,35 @@ import numpy as np
 
 
 ukb = os.environ['UKB']
-GENOME_WIDE_SIG = 5e-8
+GENOME_WIDE_TRANS_SIG = -np.log10(5e-8)
 HEIGHT_CUTOFF = 20
 
-def plot_height_snp_qq(ax):
-    snp_summary_stats = np.loadtxt(
-        (f"{ukb}/misc_data/snp_summary_stats/height/"
-         "Meta-analysis_Locke_et_al+UKBiobank_2018_UPDATED.txt"),
-        skiprows=1,
-        usecols=(8)
-    ).reshape(-1)
-    snp_summary_stats[snp_summary_stats == 0] = \
-            min(snp_summary_stats[snp_summary_stats != 0])
-    snp_summary_stats.sort()
-    trans_data = -np.log10(snp_summary_stats)
-    trans_data[trans_data > HEIGHT_CUTOFF] = HEIGHT_CUTOFF
-    uniform = (np.arange(len(trans_data)) + 0.5)/len(trans_data)
-    trans_uniform = -np.log10(uniform)
+# assume data.shape is (loci, 3)
+# where columns are chrom, pos, pval
+# will return sorted from least to most significant
+# with values in the transformed axis (-np.log10(pval))
+def prep_data(data):
+    data = data[~np.isnan(data[:, 2]), :]
+    pvals = data[:, 2]
+    pvals[pvals == 0] = min(pvals[pvals != 0])
+    pvals[:] = -np.log10(pvals)
+    pvals[pvals > HEIGHT_CUTOFF] = HEIGHT_CUTOFF
+    return data[np.argsort(data[:, 2])]
+
+# gets an array of x-axis vals of the corresponding length
+def qq_x_axis(length):
+    uniform = (np.arange(length) + 0.5)/length
+    trans_uniform = -np.log10(uniform)[::-1]
+    return trans_uniform
+
+def plot_qq(ax, prepped_pvals, label, color):
+    xs = qq_x_axis(len(prepped_pvals))
     ax.scatter(
-        trans_uniform,
-        trans_data,
-        color=colors['royalblue'],
+        xs,
+        prepped_pvals,
+        color=colors[color],
         marker='.',
-        label="SNPs genome wide"
+        label=label
     )
 
 def main():
@@ -37,66 +43,68 @@ def main():
     parser.add_argument("run_name")
     args = parser.parse_args()
 
-    results = np.loadtxt(
+    phenotypes = ['height', 'total_bilirubin']
+    #phenotypes = ['height']
+
+    results = {}
+    results['height'] = np.loadtxt(
         f'{ukb}/association/runs/{args.run_name}/results.txt',
-        skiprows=1,
-        usecols=(0, 1, 4, 6, 8, 10)
+        usecols=[0, 1, 4],
+        skiprows=1
     )
-    pvals = {}
-    pvals['height'] = results[:, 2]
-    pvals['total_bilirubin'] = results[:, 3]
-    """
-    pvals['direct_bilirubin'] = results[:, 4]
-    pvals['indirect_bilirubin'] = results[:, 5]
-    """
+    results['total_bilirubin'] = np.loadtxt(
+        f'{ukb}/association/runs/{args.run_name}/results.txt',
+        usecols=[0, 1, 6],
+        skiprows=1
+    )
+    #TODO fix nan alleles!
+    for phenotype in phenotypes:
+        results[phenotype] = prep_data(results[phenotype])
 
+    snp_summary_stats = {}
+    snp_summary_stats['height'] = np.loadtxt(
+        (f"{ukb}/misc_data/snp_summary_stats/height/"
+         "Meta-analysis_Locke_et_al+UKBiobank_2018_UPDATED.txt"),
+        usecols=(0, 1, 8),
+        skiprows=1
+    )
+    for phenotype in snp_summary_stats:
+        snp_summary_stats[phenotype] = prep_data(snp_summary_stats[phenotype])
 
-    nresults = results.shape[0]
-    uniform = (np.arange(nresults) + 0.5)/nresults
-    trans_uniform = -np.log10(uniform)
-
-    fig, axs = plt.subplots(2, figsize=(10, 10))
+    # plot QQ plots
+    n_phenotypes = len(phenotypes)
+    fig, axs = plt.subplots(n_phenotypes, figsize=(10, 5*n_phenotypes))
     fig.suptitle('QQ plots')
     fig.tight_layout(pad=5.0)
     plt_count = 0
-    for phenotype, data in pvals.items():
-        ax = axs[plt_count]
-        #ax = axs[plt_count // 2, plt_count % 2]
-        data[data == 0] = min(data[data != 0])
-        data.sort()
-        trans_data = -np.log10(data)
-        trans_data[trans_data > HEIGHT_CUTOFF] = HEIGHT_CUTOFF
+    for phenotype in phenotypes:
+        nresults = results[phenotype].shape[0]
+        if len(phenotypes) > 1:
+            ax = axs[plt_count]
+        else:
+            ax = axs
         ax.set_title(f'{phenotype}')
         ax.set_ylabel('-log_10(pval)')
         ax.set_xlabel(f'-log_10(Uniform dist) (total STR loci = {nresults})')
-        if phenotype == 'height':
-            plot_height_snp_qq(ax)
-        ax.plot(
-            trans_uniform,
-            trans_uniform,
-            color=colors['gray'],
-            label='y=x, null p-values'
-        )
-        """
-        ax.plot(
-            trans_uniform[[0, -1]],
-            [-np.log10(GENOME_WIDE_SIG)] * 2,
-            color=colors['red'],
-            label="p == 8e-5"
-        )
-        """
-        ax.scatter(
-            trans_uniform,
-            trans_data,
-            color=colors['mediumvioletred'],
-            marker='.',
-            label="STRs on chr2,3"
-        )
 
-        crosses_gws = np.argmin(data < GENOME_WIDE_SIG)
-        xy = (trans_uniform[crosses_gws],
-              trans_data[crosses_gws])
-        n_sig_loci = np.sum(data < GENOME_WIDE_SIG)
+        if phenotype in snp_summary_stats:
+            max_len = max(snp_summary_stats[phenotype].shape[0], nresults)
+            plot_qq(ax, snp_summary_stats[phenotype][:, 2],
+                    'SNPs genome wide', 'royalblue')
+        else:
+            max_len = nresults
+
+        plot_qq(ax, results[phenotype][:, 2], 'STRs on chr2,3',
+                'mediumvioletred')
+        plot_qq(ax, qq_x_axis(max_len),
+                'y=x, expectation under the null', 'gray')
+
+        crosses_gws = np.argmax(results[phenotype][:, 2]
+                                > GENOME_WIDE_TRANS_SIG)
+        xy = (qq_x_axis(nresults)[crosses_gws],
+              results[phenotype][crosses_gws, 2])
+        n_sig_loci = np.sum(results[phenotype][:, 2]
+                            > GENOME_WIDE_TRANS_SIG)
         ax.annotate(
             f"{n_sig_loci} loci with p <= 5e-8",
             xy=xy,
