a39707521dc38706f3aad8bcc146ce187cbf969c
Jonathan Margoliash <jmargolpeople@gmail.com>
2020-08-25 10:34:07 -0700

    Just started converting plot_loci.py to call R function for the actual plotting

--- association/plot_loci.py
+++ association/plot_loci.py
@@ -20,12 +20,18 @@ def main():
     args = parser.parse_args()
 
     assoc_dir = f'{ukb}/association/runs/{args.association_run_name}'
+    plot_dir = f'{assoc_dir}/locus_plots/{args.phenotype}'
+    res_col = f'{args.phenotype}_residual'
+
+    os.makedirs(plot_dir, exist_ok=True)
 
     df = pd.read_csv(
-        f'{assoc_dir}/results/{args.phenotype}.txt',
-        usecols=['id', f'{args.phenotype}_residual'],
+        f'{assoc_dir}/covars_and_phenotypes.txt',
+        usecols=['id', res_col],
         sep=' '
     )
+    data = df.to_numpy()
+    data = data[~np.isnan(data[:, 1]), :]
 
     with open(f'{assoc_dir}/phenotypes.txt') as units_file:
         first = True
@@ -33,22 +39,25 @@ def main():
             if first:
                 first = False
                 continue
-            this_phen, this_unit = line.split(":")
-            if this_phen == args.phenotype:
-                unit = this_unit
+            phen, unit = line.split(":")
+            if phen == args.phenotype:
+                with open(f'{plot_dir}/info.txt', 'w') as phenotype_info:
+                    phenotype_info.write(f'{unit}\n')
+                    break
 
-    fig, axs = plt.subplots((np.ceiling(len(args.locus)/2), 2),
-                            figsize=(10, 5*(len(args.locus)/2))
-                           )
+    nrows = (len(args.locus) + 1)//2
+    fig, axs = plt.subplots(nrows, 2, figsize=(10, 5*nrows))
     if len(axs) == 1:
         axs = np.array([[axs]])
-    fig.set_title(
+    elif len(axs.shape) == 1:
+        axs = np.array([axs])
+
+    fig.suptitle(
         f"Linear associations at individual loci against trait {args.phenotype} "
         "(covariate associations not plotted)"
     )
-
     for plot_idx, locus in enumerate(args.locus):
-        ax = axs[plot_idx % 2, plot_idx/2]
+        ax = axs[plot_idx % 2, plot_idx//2]
         chrom, pos = locus.split(":")
 
         with open(f'{assoc_dir}/results/{args.phenotype}.txt') as results:
@@ -72,16 +81,17 @@ def main():
         period = record.INFO['PERIOD']
         period = int(period)
         bpdiffs = record.INFO['BPDIFFS']
+        bpdiffs = np.array(bpdiffs)
         bpdiffs_repeat_units = bpdiffs/period
 
         idx_gts = record.genotype.array()[:, :-1]
         len_gts = np.zeros(idx_gts.shape)
         for idx, diff in enumerate(bpdiffs_repeat_units):
             len_gts[idx_gts == idx] = diff
-        avg_len_gts = np.sum(len_gts, axis=1)
+        avg_len_gts = np.sum(len_gts, axis=1)/2
         '''
         command = f"""
-        source ~/.bashrc ; 
+        source ~/.bashrc ;
         conda activate ukb ;
         bcftools query -f "REF %BPDIFFS %PERIOD" \\
             {ukb}/snpstr/info_field/chr{chrom}.vcf.gz
@@ -106,18 +116,25 @@ def main():
         axis_vals = []
         unique_gts = np.unique(avg_len_gts)
         for gt in unique_gts:
-            axis_vals.append(gt)
-            samples = vcf.samples[avg_len_gts == gt]
-            phenotypes_by_genotypes.append(
-                df.loc[samples, f'{args.phenotype}_residual']
-            )
+            samples = np.array(vcf.samples)[avg_len_gts == gt]
+            samples = np.char.partition(samples, '_')[:, 0]
+            samples = samples.astype(int)
+            vals = data[np.isin(data[:, 0], samples), 1]
+            if len(vals) == 0:
+                continue
 
-        ax.violin_plot(
+            axis_vals.append(gt)
+            phenotypes_by_genotypes.append(
+                data[np.isin(data[:, 0], samples), 1]
+            )
+        axis_vals = np.array(axis_vals)
+        print(axis_vals)
+        ax.violinplot(
             phenotypes_by_genotypes,
-            axis_vals
+            positions=axis_vals,
         )
-        ax.plot(unique_gts, unique_gts*coeff,
-                label=f"Best fit line (slope == {coeff:.2e} {unit}/repeat unit")
+        ax.plot(axis_vals, axis_vals*coeff,
+                label=f"Best fit line\n(slope == {coeff:.2e} {unit}/repeat)")
         ax.legend()
     outloc = f'{assoc_dir}/loci_plots_{"_".join(args.locus)}.png'
     outloc = "_".join(outloc.split(':'))
