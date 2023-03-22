099bd3445eefe049e8809be9ea2b4d3e7226cc4e
Jonathan Margoliash <jmargolpeople@gmail.com>
2023-03-22 15:29:15 -0700

    Make compare calls work

--- wgs/compare_calls.sh
+++ wgs/compare_calls.sh
@@ -7,40 +7,47 @@ set -euxo pipefail
 # ---- update merged file ----
 
 # liftover
-bcftools query -f'%CHROM\t%POS0\t%POS0\t%ID\n' "$MERGED_FILE" -o merged_pos.bed
-bcftools query -f'%CHROM\t%POS0\t%INFO/START\t%ID\n' "$MERGED_FILE" | \
-	awk '{print $1 "\t" $2 "\t" $3 - 1 "\t" $4}' > merged_start.bed
-bcftools query -f'%CHROM\t%POS0\t%INFO/END\t%ID\n' "$MERGED_FILE" | \
-	awk '{print $1 "\t" $2 "\t" $3 - 1 "\t" $4}' > merged_end.bed
+bcftools query -f'%CHROM\t%POS0\t%POS\t%ID\n' "$MERGED_FILE" -o merged_pos.bed
+bcftools query -f'%CHROM\t%POS0\t%INFO/START\t%ID\n' "$MERGED_FILE" -o merged_start.bed
+bcftools query -f'%CHROM\t%POS0\t%INFO/END\t%ID\n' "$MERGED_FILE" -o merged_end.bed
 
 chain=hg38ToHg19.over.chain.gz
 wget --timestamping 'ftp://hgdownload.soe.ucsc.edu/goldenPath/hg38/liftOver/hg38ToHg19.over.chain.gz' -O "$chain"
 
 for file in merged_pos.bed merged_start.bed merged_end.bed ; do 
-	liftOver $file "$chain" "$file"_hg19 "$file"_unmapped
+	liftOver $file "$chain" hg19_"$file" "$file"_unmapped
+	bgzip -f hg19_"$file"
+	tabix -f hg19_"$file".gz
 done
 
-zcat "$MERGED_FILE" | cut -f 1 > merged1.tab
-zcat "$MERGED_FILE" | cut -f 3- > merged3on.tab
-paste merged1.tab <( cut -f 2 merged_pos.bed | awk '{print $1 + 1}' )  merged3on.tab > merged.hg19.vcf.temp
-
+zcat "$MERGED_FILE" | grep -P '^##' > temp.merged.hg19.vcf
+printf "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t" >> temp.merged.hg19.vcf
 #also make samples match - ID to ID_ID
-bcftools \
-	-S <(bcftools query -l "$MERGED_FILE" | awk '{print $1 " " $1 "_" $1}' ) \
-	-a <(awk '{print $1 "\t" $2 "\t" $3 + 1}' merged_start.bed) -c CHROM,POS,INFO/START \
-	merged.hg19.vcf.temp | \
-bcftools \
-	-a <(awk '{print $1 "\t" $2 "\t" $3 + 1}' merged_end.bed) -c CHROM,POS,INFO/END \
-	- | \
+zcat "$MERGED_FILE" | grep -P '^#CHROM' | cut -f 10- | sed -e 's/\([^\t]\+\)/\1_\1/g' >> temp.merged.hg19.vcf
+zcat "$MERGED_FILE" | grep -Pv '^#' | cut -f 1 > merged1.tab
+zcat "$MERGED_FILE" | grep -Pv '^#' | cut -f 3- > merged3on.tab
+paste merged1.tab <( zcat hg19_merged_pos.bed.gz | cut -f 3 ) merged3on.tab >> temp.merged.hg19.vcf
+
+# for now match on column two and, for start, add 1 to column three
+# once the off by one error caused by HipSTR expecting 1-indexed bed files is fixed,
+# match on one plut column two and don't modify column three
+zcat hg19_merged_start.bed.gz | awk '{print $1 "\t" $2 "\t" $3 + 1}' > hg19_merged_start.annotation
+bgzip -f hg19_merged_start.annotation
+tabix -f -p bed hg19_merged_start.annotation.gz
+bcftools annotate \
+	-a hg19_merged_start.annotation.gz -c CHROM,POS,INFO/START \
+	temp.merged.hg19.vcf | \
+bcftools annotate \
+	-a hg19_merged_end.bed.gz -c CHROM,POS,INFO/END,- | \
 sed -e 's/ID=chr/ID=/' -e 's/^chr//' > merged.hg19.vcf
 # sed matches the different chromosome naming conventions, changing from chr1 to 1
 
-bgzip merged.hg19.vcf
-tabix merged.hg19.vcf.gz
+bgzip -f merged.hg19.vcf
+tabix -f merged.hg19.vcf.gz
 
 # --- update and subset imputed files
-bcftools annotate -h <( echo "##command=HipSTR" ) -o imputed_strs.reheadered.vcf.gz "$FINEMAPPED_STRS"
-tabix filtered_imputed_strs.vcf.gz
+bcftools annotate -h <( echo "##command=HipSTR" ) -o imputed_strs.reheadered.vcf.gz -O z "$FINEMAPPED_STRS"
+tabix -f imputed_strs.reheadered.vcf.gz
 
 compareSTR --ignore-phasing --balanced-accuracy --fraction-concordant-len-sum --vcf2-beagle-probabilities \
 	--vcf1 merged.hg19.vcf.gz \
